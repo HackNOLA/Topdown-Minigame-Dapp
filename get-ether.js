6dcc7a9513415afc6ac8b3654daccbb7cffe6060
Akintunde Salako Pounds <akinpds@gmail.com>
2023-02-07 16:15:29 -0600

    getting ready for web3 integration

--- get-ether.js
+++ get-ether.js
@@ -6,11 +6,13 @@ if (window.ethereum) {
   const network = "https://api.avax-test.network/ext/bc/C/rpc";
   const provider = ethers.getDefaultProvider(network);
   const address = accounts[0];
+  document.getElementById("wallet-address").innerHTML = address;
 
   const main = async () => {
     provider.getBalance(address).then((balance) => {
       // convert a currency unit from wei to ether
       const balanceInAvax = ethers.utils.formatEther(balance);
+      document.getElementById("wallet-balance").innerHTML = balanceInAvax;
       console.log(`balance: ${balanceInAvax} AVAX`);
       // balance: 2 AVAX
     });
